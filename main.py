951e7c85dafb8b7cdc96e21298920ac57dc8cce8
Katauri <katauri96@gmail.com>
2020-11-20 00:21:58 +0300

    20.11.20

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.dropdown import DropDown
 
 class ScreenManagement(ScreenManager):
 	pass
 
+class CustomDropDown(DropDown):
+    pass
+
 with open("app.kv", encoding='utf8') as f:
     app = Builder.load_string(f.read())
+
+
 
 class App(MDApp):
 	title = 'ResistorCombinator'
 
 	def build(self):
+		self.drop_menu = CustomDropDown()
 		return ScreenManagement()
 		
 	
