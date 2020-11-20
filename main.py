75fe94e203e538459d7bb7da8edfe8a5fc9c9937
Katauri <katauri96@gmail.com>
2020-11-20 07:57:00 +0300

    20.11.20

--- main.py
+++ main.py
@@ -3,24 +3,32 @@ from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.dropdown import DropDown
 
-class ScreenManagement(ScreenManager):
-	pass
-
-class CustomDropDown(DropDown):
-    pass
-
 with open("app.kv", encoding='utf8') as f:
     app = Builder.load_string(f.read())
 
 
+class ScreenManagement(ScreenManager):
+    pass
+
+
+class CustomDropDown(DropDown):
+    pass
+
+
+class PercentDropDown(DropDown):
+    pass
+
 
 class App(MDApp):
-	title = 'ResistorCombinator'
+    title = 'ResistorCombinator'
 
-	def build(self):
-		self.drop_menu = CustomDropDown()
-		return ScreenManagement()
-		
-	
-if __name__ == "__main__":				
-	App().run()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.percent_menu = CustomDropDown()
+
+    def build(self):
+        return ScreenManagement()
+
+
+if __name__ == "__main__":
+    App().run()
