ecad35495baf7ef0c8078c4a999a2d68db058f4a
Katauri <katauri96@gmail.com>
2020-11-26 07:17:52 +0300

    combine method

--- combinator.py
+++ combinator.py
@@ -1,39 +1,65 @@
 from openpyxl import load_workbook
 
 wb_val = load_workbook(filename = 'nominals.xlsx', data_only = True)
+
 sheet_E24 = wb_val['E24']
 sheet_E96 = wb_val['E96']
 sheet_E192 = wb_val['E192']
 
-#A1_val = sheet_val['A3'].value
+sheets = (sheet_E24, sheet_E96, sheet_E192)
+
+list_E24 = []
+list_E96 = []
+list_E192 = []
+
+for sheet in sheets:
+    num = 2
+    while(sheet['A' + str(num)].value):
+        if sheet == sheet_E24:
+            list_E24.append(sheet['A' + str(num)].value)
+        elif sheet == sheet_E96:
+            list_E96.append(sheet['A' + str(num)].value)
+        elif sheet == sheet_E192:
+            list_E192.append(sheet['A' + str(num)].value)
+        num += 1
+
 
 def serial_combine(value, dimension, tolerance, power, count):
     value_min = value * (1 - (tolerance / 100))
     value_max = value * (1 + (tolerance / 100))
 
     combinations_list = []
+    list_E24_pairs = []
+    list_E96_pairs = []
+    list_E192_pairs = []
+
+    tiers = [list_E24, list_E96, list_E192]
+    for tier in tiers:
+        index = 1
+        for nominal in tier:
+            if nominal >= value_max:
+                break
+            for next_nominal in tier[index:]:
+                if next_nominal >= value:
+                    break
+
+                if tier == list_E24:
+                    list_E24_pairs.append([nominal, next_nominal])
+                elif tier == list_E96:
+                    list_E96_pairs.append([nominal, next_nominal])
+                elif tier == list_E192:
+                    list_E192_pairs.append([nominal, next_nominal])
 
 
-    num = 2
-    while(sheet_E24['C' + str(num)].value):
-        next_num = num + 1
-        while(sheet_E24['C' + str(next_num)].value):
-
-            if (value_min >= (sheet_E24['C' + str(next_num)].value + sheet_E24['C' + str(num)].value)) and (value_max <= (sheet_E24['D' + str(next_num)].value + sheet_E24['D' + str(num)].value)):
-                nom_list = []
-                nom_list.append(sheet_E24['A' + str(next_num)].value)
-                nom_list.append(sheet_E24['A' + str(num)].value)
-
-                combinations_list.append(nom_list)
-
-            next_num += 1
-
-        num += 1
+            index += 1
 
 
 
+
+    print(list_E24_pairs)
     return combinations_list
 
-output = serial_combine(value = 10, dimension = 'Ом', tolerance = 5, power = 0, count = 2)
+
+output = serial_combine(value = 5, dimension = 'Ом', tolerance = 5, power = 0, count = 2)
 print(output)
 
