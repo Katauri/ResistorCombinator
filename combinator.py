9bacb7725db1272d3b1792f4f03ea5e48ae1ada5
Katauri <katauri96@gmail.com>
2020-11-28 16:50:01 +0300

    recode combinator method

--- combinator.py
+++ combinator.py
@@ -11,53 +11,45 @@ sheets = (sheet_E24, sheet_E96, sheet_E192)
 list_E24 = []
 list_E96 = []
 list_E192 = []
+list_nominals = []
 
 for sheet in sheets:
     num = 2
     while(sheet['A' + str(num)].value):
+
         if sheet == sheet_E24:
             list_E24.append(sheet['A' + str(num)].value)
+            list_nominals.append((sheet['A' + str(num)].value, 5))
         elif sheet == sheet_E96:
             list_E96.append(sheet['A' + str(num)].value)
+            list_nominals.append((sheet['A' + str(num)].value, 1))
         elif sheet == sheet_E192:
             list_E192.append(sheet['A' + str(num)].value)
+            list_nominals.append((sheet['A' + str(num)].value, 0.5))
         num += 1
 
+list_nominals.sort(key=lambda x: x[::1])
 
 def serial_combine(value, dimension, tolerance, power, count):
     value_min = value * (1 - (tolerance / 100))
     value_max = value * (1 + (tolerance / 100))
 
     combinations_list = []
-    list_E24_pairs = []
-    list_E96_pairs = []
-    list_E192_pairs = []
-
     tiers = [list_E24, list_E96, list_E192]
-    for tier in tiers:
-        index = 0
-        for nominal in tier:
-            if nominal >= value_max:
-                break
-            for next_nominal in tier[index:]:
-                if next_nominal >= value_max:
-                    break
 
-                if tier == list_E24:
-                    list_E24_pairs.append([nominal, next_nominal])
-                elif tier == list_E96:
-                    list_E96_pairs.append([nominal, next_nominal])
-                elif tier == list_E192:
-                    list_E192_pairs.append([nominal, next_nominal])
-            index += 1
+    count_list = []
+    for i in range(count):
+        count_list.append(1)
+
+    for index in count_list:
+        for i
 
 
 
-
-    print(list_E192_pairs)
+    print(list_nominals)
     return combinations_list
 
 
-output = serial_combine(value = 5, dimension = 'Ом', tolerance = 5, power = 0, count = 2)
+output = serial_combine(value = 5, dimension = 'Ом', tolerance = 5, power = 0, count = 4)
 print(output)
 
